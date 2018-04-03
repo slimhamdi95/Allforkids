8b842449c7a64a0df577809e3166af8a62939677
Firas Kordoghli <firas9@rocketmail.com>
2018-04-03 23:02:28 +0100

    r

--- src/AllForKids/DivertissementBundle/Controller/DefaultController.php
+++ src/AllForKids/DivertissementBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('AllForKidsDivertissementBundle:Default:index.html.twig');
+        return $this->render('AllForKidsDivertissementBun   dle:Default:index.html.twig');
     }
 }
