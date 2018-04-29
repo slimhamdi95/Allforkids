5d685cc962792d56d0391cf2d5fc37e44300c31f
rayen4r <36058081+rayen4r@users.noreply.github.com>
2018-04-30 00:42:36 +0200

    rayen

--- src/AllForKids/EntityBundle/Controller/DefaultController.php
+++ src/AllForKids/EntityBundle/Controller/DefaultController.php
@@ -3,6 +3,9 @@
 namespace AllForKids\EntityBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class DefaultController extends Controller
 {
@@ -10,4 +13,25 @@ class DefaultController extends Controller
     {
         return $this->render('AllForKidsEntityBundle:Default:layout.html.twig');
     }
+
+
+
+    /************* service user mobile **************************/
+
+
+
+    public function FindByLoginAction($userName)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $ev = $em->getRepository('AllForKidsEntityBundle:User')->findOneBy(['username' => $userName]);
+        $ser = new Serializer([new ObjectNormalizer()]);
+        $formated = $ser->normalize($ev);
+        return new JsonResponse($formated);
+    }
+
+
+   
+
+
+
 }
--- src/ForumBundle/Controller/DefaultController.php
+++ src/ForumBundle/Controller/DefaultController.php
@@ -3,6 +3,9 @@
 namespace ForumBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class DefaultController extends Controller
 {
@@ -10,4 +13,7 @@ class DefaultController extends Controller
     {
         return $this->render('ForumBundle:Default:index.html.twig');
     }
+
+
+
 }
